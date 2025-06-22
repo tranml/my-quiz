a1bed2e6cfd00a81edf0a978717103aaf2d93804
TranML <the44secrets@gmail.com>
2025-06-22 09:42:24 +0700

    react-native-safe-area-context

--- App.tsx
+++ App.tsx
@@ -1,13 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import QuizScreen from './src/app/QuizScreen';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import QuizScreen from "./src/app/QuizScreen";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <>
+    <SafeAreaProvider>
       <QuizScreen />
       <StatusBar style="auto" />
-    </>
+    </SafeAreaProvider>
   );
 }
-
