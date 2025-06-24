4bee3e6b9235e874d2072bd41a3e869afc6c239f
TranML <the44secrets@gmail.com>
2025-06-24 07:02:06 +0700

    QuizProvider: handle endgame

--- src/providers/QuizProvider.tsx
+++ src/providers/QuizProvider.tsx
@@ -32,7 +32,20 @@ export const QuizProvider = ({ children }: PropsWithChildren) => {
   const [selectedOption, setSelectedOption] = useState<string | undefined>();
   const [score, setScore] = useState(0);
 
+  const isFinished = questionIndex >= questions.length;
+
+  const restart = () => {
+    setQuestionIndex(0);
+    setScore(0);
+    setSelectedOption(undefined);
+  };
+
   const onNext = () => {
+    if (isFinished) {
+      restart();
+      return;
+    }
+
     if (selectedOption === question?.correctAnswer) {
       setScore((currentScore) => currentScore + 1);
     }
