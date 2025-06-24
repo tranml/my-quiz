427181e4a3f6f8933f794f321e010b8c3f1911e1
TranML <the44secrets@gmail.com>
2025-06-24 08:07:41 +0700

    QuestionCard: clean unused comments

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -2,19 +2,12 @@ import { View } from "react-native";
 import AnswerOption from "./AnswerOption";
 import { Question } from "../types";
 import Card from "./Card";
-import { useState } from "react";
 
 type QuestionCardProps = {
   question: Question;
 };
 
 export default function QuestionCard(props: QuestionCardProps) {
-  // const [selectedOption, setSelectedOption] = useState<string | undefined>();
-
-  // const onOptionSelected = (option: string) => {
-  //   console.warn(option, "selected");
-  //   setSelectedOption(option);
-  // };
 
   return (
     <Card title={props.question.title}>
@@ -23,8 +16,6 @@ export default function QuestionCard(props: QuestionCardProps) {
           <AnswerOption
             key={option + index}
             option={option}
-            // isSelected={selectedOption === option}
-            // onPress={() => onOptionSelected(option)}
           />
         ))}
       </View>
